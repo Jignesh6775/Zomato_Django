030b6b01bcc9b03e23fc303650701621ce08a722
Jignesh <jigsvadiyatar6557@gmail.com>
2023-08-15 22:19:52 +0530

    Backend is ready

---
+++ Zomato_Backend/menu_route/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+import json
+from django.http import JsonResponse, HttpResponse
+# Create your views here.
+from .models import FoodMenu
+
+
+def Create(request):
+    if request.method == "POST":
+        body = json.loads(request.body)
+        foodname = body["foodname"]
+        price = body["price"]
+        available = body["available"]
+        menu = FoodMenu.objects.create(
+            foodname=foodname, price=price, available=available)
+    else:
+        return HttpResponse(json.dumps({"msg": "wrong routes"}))
+    return HttpResponse(json.dumps({"msg": "Data Posted succesfully"}))
+
+
+def Get(req):
+    menu = FoodMenu.objects.all()
+    arr = {"data": list(menu.values())}
+    return JsonResponse(arr)
+
+
+def Update(request, itemid):
+    print(type(itemid))
+    if request.method == "PATCH":
+        menu = FoodMenu.objects.get(id=itemid)
+        menu.available = "yes"
+        menu.save()
+    else:
+        return JsonResponse({"msg": "eror"})
+    return JsonResponse({"msg": "Update"})
+
+
+def Delete(request, itemid):
+    if request.method == "DELETE":
+        menu = FoodMenu.objects.get(id=itemid)
+        menu.delete()
+    else:
+        return JsonResponse({"msg": "eror"})
+    return HttpResponse(json.dumps({"msg": "Deleted"}))
